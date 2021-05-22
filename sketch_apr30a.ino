f1a53fe56f421d183ca8bba6d2457a3eaf45fb7d
Florin Măruț <50501751+florinmarut@users.noreply.github.com>
2021-05-22 12:40:33 +0300

    Added sketch file.

---
+++ sketch_apr30a.ino
@@ -0,0 +1,56 @@
+#include <Servo.h>
+
+const int trigPin = 13;
+const int echoPin = 12;
+
+long duration;
+int distance;
+
+Servo myServo;
+
+int pos = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  myServo.attach(8);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(pos = 0; pos <= 180; pos += 1){
+    distance = calculateDistance();
+    myServo.write(pos);
+    Serial.print("(");
+    Serial.print(pos);
+    Serial.print(", ");
+    Serial.print(distance);
+    Serial.print(")");
+    delay(15);
+  }
+
+  for(pos = 180; pos >= 0; pos -= 1){
+    distance = calculateDistance();
+    myServo.write(pos);
+    Serial.print("(");
+    Serial.print(pos);
+    Serial.print(", ");
+    Serial.print(distance);
+    Serial.print(")");
+    delay(15);
+  }
+}
+
+int calculateDistance(){
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*0.034/2;
+  return distance;
+}
